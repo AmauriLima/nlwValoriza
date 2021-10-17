6355ac88361f38efac58906701563178a1d273c6
AmauriLima <amauri.plimaj@gmail.com>
2021-10-17 18:42:53 -0300

    feat: adding column password to User table

--- src/app/controllers/UserController.ts
+++ src/app/controllers/UserController.ts
@@ -1,27 +1,39 @@
 import { Request, Response } from 'express';
 import { getCustomRepository } from 'typeorm';
+import bcrypt from 'bcryptjs';
 import usersRepository from '../repositories/UsersRepository';
 
 class UserController {
   async store(request: Request, response: Response) {
     const UsersRepository = getCustomRepository(usersRepository);
-    const { name, email, admin } = request.body;
+    const {
+      name, email, admin, password,
+    } = request.body;
 
-    const userAlreadyExists = await UsersRepository.findByEmail(email);
+    const userAlreadyExists = await UsersRepository.findOne({
+      email,
+    });
 
     if (!email) {
       return response.status(400).json({ error: 'E-mail is required' });
+    }
+
+    if (!password) {
+      return response.status(400).json({ error: 'Password is required' });
     }
 
     if (userAlreadyExists) {
       return response.status(400).json({ error: 'This e-mail is already in use' });
     }
 
+    const encryptedPassword = await bcrypt.hash(password, 10);
+
     const user = UsersRepository.create({
-      name, email, admin,
+      name, email, admin, password: encryptedPassword,
     });
 
     await UsersRepository.save(user);
+    user.password = undefined;
 
     response.status(201).json(user);
   }
--- src/app/repositories/UsersRepository.ts
+++ src/app/repositories/UsersRepository.ts
@@ -2,11 +2,6 @@ import { EntityRepository, Repository } from 'typeorm';
 import { User } from '../../entities/User';
 
 @EntityRepository(User)
-class UsersRepository extends Repository<User> {
-  async findByEmail(email: string) {
-    const user = await this.findOne({ email });
-    return user;
-  }
-}
+class UsersRepository extends Repository<User> {}
 
 export default UsersRepository;
